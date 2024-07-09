21a265954e0267c96a20e64562e12753e6c953f3
Ilhom <ilhom@example.com>
2024-07-09 23:01:23 +0500

    homework

--- src/routes/single-page/SinglePage.jsx
+++ src/routes/single-page/SinglePage.jsx
@@ -2,28 +2,33 @@ import "./Single.css"
 import {useParams} from "react-router-dom";
 import {useFetch} from "../../hooks/useFetch.jsx";
 import {FaStar} from "react-icons/fa6";
+import Container from "../../components/container/Container.jsx";
+import Navbar from "../../components/navbar/Navbar.jsx";
 
 const SinglePage = () => {
     const productId = useParams()
     const [data, loading] = useFetch(`/products/${productId.id}`)
 
     return (
-        <div>
-            <div>
-                <img src={data.thumbnail} alt=""/>
-            </div>
-            <div className="card-body">
-                <h2>{data.title}</h2>
-                <div className="price-rating">
-                    <p className="rating"><span><FaStar/></span> {data.rating}</p>
-                    <div>
-                        <span className="discount">${(data.price - (data.price * data.discountPercentage) / 100).toFixed(2)}</span>
-                        <span className="price">${data.price}</span>
-                    </div>
+        <Container>
+            <Navbar/>
+            <div className="wrapper">
+                <div className="single-img">
+                    <img src={data.thumbnail} alt={data.title}/>
                 </div>
-                <p className="desc">{data.description}</p>
+                <div className="card-body single-body">
+                    <h2>{data.title}</h2>
+                    <div className="price-rating p">
+                        <p className="rating"><span><FaStar/></span> {data.rating}</p>
+                        <div className='prices'>
+                            <span className="discount">${(data.price - (data.price * data.discountPercentage) / 100).toFixed(2)}</span>
+                            <span className="price">${data.price}</span>
+                        </div>
+                    </div>
+                    <p className="desc single-desc">{data.description}</p>
+                </div>
             </div>
-        </div>
+        </Container>
     )
 }
 export default SinglePage
